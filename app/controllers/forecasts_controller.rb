3d93bc3de8f906ea517ecf5a75909a2eeb509bb7
Jay Wolff <jaywolff16@gmail.com>
2025-01-10 00:56:30 -0500

    fix a bug with error alert showing on page load b4 a search is performed

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,6 +1,10 @@
 class ForecastsController < ApplicationController
   def index
-    @forecast = WeatherApiService.get_forecast(params[:address])
-    flash.now[:alert] = @forecast.errors.full_messages if @forecast.errors.any?
+    if params.has_key?(:address)
+      @forecast = WeatherApiService.get_forecast(params[:address])
+      flash.now[:alert] = @forecast.errors.full_messages if @forecast.errors.any?
+    else
+      @forecast = Forecast.new
+    end
   end
 end
